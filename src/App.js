d2c9542c3d5b6265eba0cc3ebed4ea65e5436cef
yancn21th <yancn21th@gmail.com>
2022-02-13 00:03:30 +0800

    basic style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import logo from "./logo.png";
+import sanya from "./sanya.jpeg";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="p-8">
+      <img className="h-10" src={logo} alt="" />
+      <img className="mt-6 rounded-lg shadow-xl" src={sanya} alt="" />
+      <h1 className="mt-6 text-2xl font-bold text-gray-900">
+        好好<span className="text-indigo-500">工作</span>，度假就到这里来耍
+      </h1>
+      <p className="mt-2 text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas,
+        impedit quam amet dolor eius eum soluta dicta velit! Incidunt facilis
+        architecto dolorem dicta reiciendis fuga odio qui ducimus facere
+        expedita?
+      </p>
+
+      <div className="mt-4">
+        <a
+          className="inline-block rounded-lg shadow-xl bg-indigo-500 text-white px-5 py-3 tracking-wider font-semibold text-sm"
+          href="/#"
+        >
+          请年假
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
